26b8e5137cd6e849e82168cd2d01e78256a03fd7
Goma0925 <amonbali0925@gmail.com>
2020-08-23 12:47:42 -0400

    Merged the features of MapperExecutor and Mapper interface. Added a test for the new mapper

--- src/main/java/adoop/Mapper.java
+++ src/main/java/adoop/Mapper.java
@@ -1,5 +1,98 @@
 package adoop;
 
-public interface Mapper {
-	void map(String key, String value, Context context);
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Map;
+
+import exceptions.InvalidMapperException;
+import filehandler.DataLoader;
+import settings.SystemPathSettings;
+
+public abstract class Mapper implements Runnable {
+	protected String workerId = null;
+	protected int startIndex = 0;
+	protected int endIndex = 0;
+	protected File inputFile = null;
+	protected Context resultContext = null; 
+	protected SystemPathSettings pathSetting = null;
+	
+	public Mapper(String workerId, SystemPathSettings pathSettings, File inputFile, int startIndex, int endIndex) {
+		this.workerId = workerId;
+		this.startIndex = startIndex;
+		this.endIndex = endIndex;
+		this.inputFile = inputFile;
+		this.pathSetting = pathSettings;
+	}
+
+	public void run() {
+		//Run mapping
+		try {
+			this.runMap();
+		} catch (InstantiationException e1) {
+			e1.printStackTrace();
+		} catch (IllegalAccessException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		//Write results to file
+		try {
+			this.writeToFiles();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	};
+	
+	public void runMap() throws InstantiationException, IllegalAccessException, IOException {
+		System.out.println(this.workerId + ":Running process...");
+		Context tempoContext = new Context();
+		DataLoader loader = new DataLoader();
+		String[] inputLines = null;
+		int chunkStartIndex = this.startIndex;
+		System.out.println(this.workerId + ":Loading a chunk["+Integer.toString(startIndex) + ","+Integer.toString(endIndex)+"]...");
+		//Read the input file
+		inputLines = loader.loadChunkByLineIndices(inputFile, startIndex, endIndex);
+		System.out.println(this.workerId + ":Done loading a chunk of size:" + Integer.toString(inputLines.length));
+		//Map process
+		for (int i=0; i<inputLines.length; i++) {
+			this.map(Integer.toString(chunkStartIndex), inputLines[i], tempoContext);
+		}
+		System.out.println(this.workerId + ":Done processing:" + Integer.toString(inputLines.length));
+		this.resultContext  = tempoContext;
+	};
+	
+	private void writeToFiles() throws IOException {
+		//Write the results to files. Each key's associated values will be written in different files.
+		System.out.println(this.workerId + ":Writing to file.. :" + this.pathSetting.mapOutputBaseDir.toString());
+		String key;
+		Path keyDir;
+		ArrayList<String> valueList;
+		SystemPathSettings pathSettings = this.pathSetting;
+		for (Map.Entry<String, ArrayList<String>> entry : this.resultContext.getMap().entrySet()) {
+	        key = entry.getKey();
+	        valueList = entry.getValue();
+	        keyDir = Paths.get(pathSettings.mapOutputBaseDir.toString() + "/"+ key);
+	        if (!Files.exists(keyDir)){
+	        	keyDir.toFile().mkdir();
+	        }
+	        File outputFile = new File(keyDir.toFile(), pathSettings.getMapOutputFileName(key, this.workerId));
+			FileWriter fr = new FileWriter(outputFile, true);
+			BufferedWriter br = new BufferedWriter(fr);
+			br.write(key);
+			for (String value: valueList) {
+				br.write("\n"+value);;
+			}
+			br.close();
+			fr.close();
+	    };
+	    
+	}
+
+	public abstract void map(String key, String value, Context context);
 }
--- src/test/java/adooptest/MapperTest.java
+++ src/test/java/adooptest/MapperTest.java
@@ -9,16 +9,15 @@ import java.util.ArrayList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import adoop.MapExecutor;
+import adoop.Mapper;
 import exceptions.InvalidMapperException;
 import filehandler.DataLoader;
 import filehandler.FileSystemManager;
 import settings.SystemPathSettings;
-import test_usermodules.InvalidClass;
 import test_usermodules.TestMapper;
 import testsettings.TestPathSettings;
 
-class MapExecutorTest {
+class MapperTest {
 	SystemPathSettings pathSettings = new TestPathSettings();
 	FileSystemManager fileSystemManager = new FileSystemManager(this.pathSettings);
 	@Test
@@ -26,6 +25,9 @@ class MapExecutorTest {
 		//Set up the file storage
 		this.fileSystemManager.initFileSystem();
 		this.fileSystemManager.clearMapOutputDir();
+		File outputDir = pathSettings.mapOutputBaseDir.toFile();
+		File outputFile = null;
+
 		
 		int startIndex = 0;
 		int endIndex = 50;
@@ -33,11 +35,10 @@ class MapExecutorTest {
 		String mapperId = "Test-ID";
 		File inputFile = new File(path);
 		DataLoader loader = new DataLoader();
-		MapExecutor mExcecutor = new MapExecutor(mapperId, TestMapper.class, this.pathSettings, inputFile, startIndex, endIndex);
-		mExcecutor.run();
+		Mapper mapper = new TestMapper(mapperId, this.pathSettings, inputFile, startIndex, endIndex);
+		mapper.run();
 		
-		File outputDir = pathSettings.mapOutputBaseDir.toFile();
-		File outputFile = null;
+		//Check output
 		int outputLineLength = -1;
 		String key = null;
 		//The input.csv file contains 5 different keys. 5 files should be created for each key and 
@@ -54,21 +55,6 @@ class MapExecutorTest {
 			}
 			//Check if the mapper output contains the correct number of values that should be in the output
 			Assertions.assertEquals(outputLines.size(), 11);
-		}
-	}
-	
-	@Test
-	void passInvalidMapper(){
-		String path = "src/test/resources/input.csv";
-		File inputFile = new File(path);
-		int startIndex = 0;
-		int endIndex = 50;
-		try {
-			@SuppressWarnings("unused")
-			MapExecutor mExcecutor = new MapExecutor("Test-ID", InvalidClass.class, this.pathSettings, inputFile, startIndex, endIndex);
-			fail("	MapExecutor failed to catch an exception when being passed a non-mapper class");
-		} catch (InvalidMapperException e) {
-			System.out.println("	MapExecutor successfully raised an error when being passed a non-mapper class.");
 		}
 	}
 
