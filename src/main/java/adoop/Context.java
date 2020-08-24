32b888b93691aeb8a4f2802e07c6ce98e8234e1c
Goma0925 <amonbali0925@gmail.com>
2020-08-24 18:35:54 -0400

    Upgraded Context class and implemented its test.

--- src/main/java/adoop/Context.java
+++ src/main/java/adoop/Context.java
@@ -7,29 +7,51 @@ import java.util.Map;
 import java.util.Set;
 
 public class Context {
-	String delimiter = null; //Delimiter to separate key & value pairs in the mapper output file.
-	Map<String, ArrayList<String>> keyValMapping = new HashMap<String, ArrayList<String>>();
-	
-	public Context() {
-		this.delimiter = ",";
-	}
-	
-	public Context(String delimiter) {
-		this.delimiter = delimiter;
+	//Context class allows Mapper and Reducer to communicate with the main system. 
+	//It allows them to write and (key:value) pair.
+	//Each writing is directed to a certain name space. A key and value to different name spaces are
+	//managed separately and the result mapping for the name space can be retrieved by getMapping method.
+	String defaultNameSpace = null;
+	Map<String, Map<String, ArrayList<String>>> keyValMappingByNamespace = new HashMap<String, Map<String, ArrayList<String>>> ();
+
+	public Context(String defaultNameSpace) {
+		this.defaultNameSpace = defaultNameSpace;
+		keyValMappingByNamespace.put(defaultNameSpace, new HashMap<String, ArrayList<String>>());
 	}
 
 	public void write(String key, String value) {
-		if (!this.keyValMapping.containsKey(key)) {
-			ArrayList<String> list = new ArrayList<String>();
-			list.add(value);
-			this.keyValMapping.put(key, list);
-		}
-		else {
-			this.keyValMapping.get(key).add(value);
-		}
+		this.addPairToMapping(this.keyValMappingByNamespace.get(this.defaultNameSpace), key, value);
 	};
 	
-	public Map<String, ArrayList<String>> getMap(){
-		return this.keyValMapping;
+	private void addPairToMapping(Map<String, ArrayList<String>> keyValMapping, String key, String value) {
+		if (!keyValMapping.containsKey(key)) {
+			ArrayList<String> list = new ArrayList<String>();
+			list.add(value);
+			keyValMapping.put(key, list);
+		}
+		else {
+			keyValMapping.get(key).add(value);
+		};
+	}
+	
+	public Map<String, ArrayList<String>> getMapping(String nameSpace){
+		return this.keyValMappingByNamespace.get(nameSpace);
+	};
+	
+	public Map<String, ArrayList<String>> getMapping(){
+		//Return the mapping associated with the defaultNameSpace if no nameSpace is given. 
+		return this.keyValMappingByNamespace.get(this.defaultNameSpace);
+	}
+
+	public void writeToNameSpace(String nameSpace, String key, String value, String baseOutputPath) {
+		Map<String, ArrayList<String>> keyValMapping = this.keyValMappingByNamespace.get(nameSpace);;
+		if (keyValMapping != null) {
+			this.addPairToMapping(keyValMapping, key, value);
+		}else {
+			keyValMapping = new HashMap<String, ArrayList<String>>();
+			this.addPairToMapping(keyValMapping, key, value);
+			this.keyValMappingByNamespace.put(nameSpace, keyValMapping);
+		}
+		
 	} 
 }
---
+++ src/test/java/adooptest/ContextTest.java
@@ -0,0 +1,107 @@
+package adooptest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import adoop.Context;
+
+class ContextTest {
+	String[] testValueSet1 = {"value1", "value2", "value3", "value4", "value5",
+						"value6", "value7", "value8", "value9", "value10"};
+	String[] testValueSet2 = {"hello1", "hello2", "hello3", "hello4", "hello5", 
+						"hello6", "hello7", "hello8", "hello9", "hello10", };
+	String[] testValueSet3 = {"hi1", "hi2", "hi3", "hi4", "hi5", 
+						"hi6", "hi7", "hi8", "hi9", "hi10", };
+	String[][] valueSets = {testValueSet1, testValueSet2, testValueSet3};
+	
+	@Test
+	void testWrite() {
+		String defaultNameSpace = "DEFAULT";
+		Context context = new Context(defaultNameSpace);
+		
+		//Write to the default output name space
+		String key = "";
+		for (int i=0; i<testValueSet1.length; i++) {
+			//Put items at an odd number index with key "1", items at an even number index with key "0"
+			key = Integer.toString(i%2);
+			context.write(key, testValueSet1[i]);
+		}
+		
+		//Get the map of a default name space that stores the written results.
+		Map<String, ArrayList<String>> keyValMapping = context.getMapping();
+		
+		//Check the results for key "0"
+		key = "0";
+		ArrayList<String> valueList = keyValMapping.get(key);
+		ArrayList<String> answerValueList = getItemsAtEvenIndex(testValueSet1);
+		Assertions.assertArrayEquals(answerValueList.toArray(), valueList.toArray());
+		
+		//Check the results for key "1"
+		key = "1";
+		valueList = keyValMapping.get(key);
+		answerValueList = getItemsAtOddIndex(testValueSet1);
+		Assertions.assertArrayEquals(answerValueList.toArray(), valueList.toArray());
+	};
+	
+	@Test
+	void testWriteToNamedOutput(){
+		ArrayList<String> nameSpaces = new ArrayList<String>();
+		String defaultNameSpace = "DEFAULT";
+		Context context = new Context(defaultNameSpace);
+		String key = "";
+		for (int setNum=0; setNum<valueSets.length; setNum++) {
+			String nameSpace = "Set" + Integer.toString(setNum);
+			nameSpaces.add(nameSpace);
+			for (int i=0; i<valueSets[setNum].length; i++) {
+				//Put items at an odd number index with key "1", items at an even number index with key "0"
+				key = Integer.toString(i%2);
+				context.writeToNameSpace(nameSpace, key, valueSets[setNum][i], "/"+nameSpace);				
+			}
+		};
+		
+		//Check if each name space contains all the values for the corresponding set.
+		for (int setNum=0; setNum<valueSets.length; setNum++) {
+			//Get the keyValMapping for each space.
+			String nameSpace = "Set" + Integer.toString(setNum);
+			Map<String, ArrayList<String>> keyValMapping = context.getMapping(nameSpace);
+			
+			//Check the results for key "0"
+			key = "0";
+			ArrayList<String> valueList = keyValMapping.get(key);
+			ArrayList<String> answerValueList = getItemsAtEvenIndex(valueSets[setNum]);
+			Assertions.assertArrayEquals(answerValueList.toArray(), valueList.toArray());
+			
+			//Check the results for key "1"
+			key = "1";
+			valueList = keyValMapping.get(key);
+			answerValueList = getItemsAtOddIndex(valueSets[setNum]);
+			Assertions.assertArrayEquals(answerValueList.toArray(), valueList.toArray());
+		}
+	}
+	
+	ArrayList<String> getItemsAtOddIndex(String[] array) {
+		ArrayList<String> results = new ArrayList<String>();
+		for(int j=0; j != array.length; j++) {
+		    if (j % 2 == 1) { // Odd
+		    	results.add(array[j]); 
+		    }
+		}
+		return results;
+	}
+	
+	ArrayList<String> getItemsAtEvenIndex(String[] array) {
+		ArrayList<String> results = new ArrayList<String>();
+		for(int j=0; j != array.length; j++) {
+		    if (j % 2 == 0) { // Even
+		    	results.add(array[j]); 
+		    }
+		}
+		return results;
+	}
+
+}
