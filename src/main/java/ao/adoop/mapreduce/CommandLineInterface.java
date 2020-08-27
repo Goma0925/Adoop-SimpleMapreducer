d22ec05a0a301df964340fe9b233f2ff8b7084e5
Goma0925 <amonbali0925@gmail.com>
2020-08-27 18:16:15 -0400

    Implemented Timer class and CommandLineInterface class

--- src/main/java/ao/adoop/mapreduce/CommandLineInterface.java
+++ src/main/java/ao/adoop/mapreduce/CommandLineInterface.java
@@ -1,5 +1,31 @@
 package ao.adoop.mapreduce;
 
-public class CommandLineInterface implements UserInterface {
+import javafx.util.Pair;
 
+public class CommandLineInterface implements  UserInterface{
+	public void displayRunTime(String label, double runTime) {
+	    System.out.println(label + Double.toString(runTime));
+	}
+
+	public void doMappingEnd() {
+        System.out.println("Mapping finished.");
+	}
+
+	public void doMappingStart(int numberOfThreads) {
+		System.out.println("---Concurrent Mapping start---");
+		System.out.println("	Mapping running on " + Integer.toString(numberOfThreads) + " threads.");
+	}
+
+	public void doReducingStart(int numberOfThreads) {
+		System.out.println("---Concurrent Reducing start---");
+		System.out.println("	Reducing running on " + Integer.toString(numberOfThreads) + " threads.");
+	};
+
+	public void doReducingEnd() {
+        System.out.println("Reducing finished.");
+	}
+
+	public Pair<String, String[]> acceptCommand() {
+		return null;
+	};
 }
--- src/main/java/ao/adoop/mapreduce/Timer.java
+++ src/main/java/ao/adoop/mapreduce/Timer.java
@@ -1,5 +1,22 @@
 package ao.adoop.mapreduce;
 
-public class Timer {
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 
+public class Timer {
+	private ThreadMXBean TMB = ManagementFactory.getThreadMXBean();
+	private long startTime;
+	private long endTime;
+		
+	public void startCpuTimer() {
+		this.startTime = TMB.getCurrentThreadCpuTime();
+	}
+	
+	public void stopCpuTimer() {
+		this.endTime = TMB.getCurrentThreadCpuTime();
+	};
+	
+	public double getCpuTimer() {
+	    return (double) (endTime - startTime) / 1000000000;
+	};
 }
